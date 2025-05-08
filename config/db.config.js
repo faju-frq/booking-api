5118102bf115c7d845323c710078ea5f581d3017
Muhammed Farzin Farooque T K <mfarooquetk@gmail.com>
2025-05-08 19:09:50 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -7,7 +7,7 @@ const dbConfig = {
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
   dialect: process.env.DB_DIALECT || "mysql",
+  MYSQL_URL: process.env.MYSQL_URL,
 };
-console.log(process.env.DB_HOST);
-console.log("DB Config:", dbConfig);
+
 export default dbConfig;
